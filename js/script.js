ff20cb261596208b1866eaf3cdc659505e989bc9
Torbjørn Haukås <69842749+Hawkas@users.noreply.github.com>
2021-05-21 23:39:54 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
-const url = "https://fronthauk.com/blogposts/wp-json/wp/v2/posts";
-
+const cors = "https://noroffcors.herokuapp.com/";
+const url = cors + "https://fronthauk.com/blogposts/wp-json/wp/v2/posts/11";
+let post;
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    post = data;
+    return fetch(cors + "https://fronthauk.com/blogposts/wp-json/wp/v2/media?parent=" + post.id);
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(post, data);
+    let images = sortImages(data);
     document.body.innerHTML = `
-    <h1>${data.title.rendered}</h1>
-    ${data.content.rendered}
-    <strong>Post ID: ${data.id}</strong>
-    <br /><strong>Date: ${data.date}</strong>
-      <h1>${data.title.rendered}</h1>
-      ${data.content.rendered}
-      <strong>Post ID: ${data.id}</strong><br />
-      <strong>Date: ${data.date}</strong><br />
+    <img src="${images.mobile}" alt="${images.alt}" srcset="">
+    <h1>${post.title.rendered}</h1>
+    ${post.content.rendered}
+    <strong>Post ID: ${post.id}</strong>
+    <br /><strong>Date: ${post.date}</strong>
+      <h1>${post.title.rendered}</h1>
+      ${post.content.rendered}
+      <strong>Post ID: ${post.id}</strong><br />
+      <strong>Date: ${post.date}</strong><br />
       `;
   })
   .catch((error) => console.error(error));
+function sortImages(images) {
+  let imgSorted = { mobile: "", desktop: "", alt: images[0].alt_text };
+  for (img of images) {
+    if (img["media_details"].width === 640) imgSorted.mobile = img.source_url;
+    if (img["media_details"].width === 1440) imgSorted.desktop = img.source_url;
+  }
+  return imgSorted;
+}
