421fdcf45249ff664d16dd04d1fba0d64f674056
Torbjørn Haukås <69842749+Hawkas@users.noreply.github.com>
2021-05-21 19:11:20 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
-const url = "https://fronthauk.com/blogposts/wp-json/wp/v2/posts/";
+const url = "https://fronthauk.com/blogposts/wp-json/wp/v2/posts";
 
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
     document.body.innerHTML = `
-    <h1>${data[1].title.rendered}</h1>
-    ${data[1].content.rendered}
-    <strong>Post ID: ${data[1].id}</strong>
-    <br /><strong>Date: ${data[1].date}</strong>
-      <h1>${data[0].title.rendered}</h1>
-      ${data[0].content.rendered}
-      <strong>Post ID: ${data[0].id}</strong><br />
-      <strong>Date: ${data[0].date}</strong><br />
-      <br /><br /><small>---------------------------Messages below are not from the API---------------------------</small>
-      <br /><small>Everything works as it should</small><br />
-      <small>Now excuse me while I spend the next few weeks picking colors and stock photos</small>
+    <h1>${data.title.rendered}</h1>
+    ${data.content.rendered}
+    <strong>Post ID: ${data.id}</strong>
+    <br /><strong>Date: ${data.date}</strong>
+      <h1>${data.title.rendered}</h1>
+      ${data.content.rendered}
+      <strong>Post ID: ${data.id}</strong><br />
+      <strong>Date: ${data.date}</strong><br />
       `;
   })
   .catch((error) => console.error(error));
