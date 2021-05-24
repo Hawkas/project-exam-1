1af8eb35c0a66629bf5e572fd800a0e2296f1307
Torbjørn Haukås <69842749+Hawkas@users.noreply.github.com>
2021-05-24 13:52:22 +0200

    finally done with the finicky stuff

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 console.log(window.screen.width);
 const menuBar = document.querySelector(".navblock__bars");
-menuBar.addEventListener("click", openMenu);
+const listBlock = document.querySelector(".navblock__lower");
+const anchorArray = document.querySelectorAll("a");
+let resizeTimer;
+
+/* To prevent jankiness from link click events */
+function noBubble(e) {
+  e.stopPropagation();
+}
+/* Open dropdown menu and animate bars */
 function openMenu() {
-  const listBlock = document.querySelector(".navblock__lower");
   menuBar.classList.toggle("switch");
   listBlock.classList.toggle("hide");
 }
+// Hide menu when pressing on the opaque bg
+listBlock.addEventListener("click", function () {
+  if (document.querySelector(".hide")) {
+    openMenu();
+  }
+});
+
+menuBar.addEventListener("click", openMenu);
+for (anchor of anchorArray) {
+  anchor.addEventListener("click", noBubble);
+}
 
 // This one doesnt do anything but prevent the dropdown menu from appearing when resizing the window, and was taken from https://css-tricks.com/stop-animations-during-window-resizing/
-let resizeTimer;
+
 window.addEventListener("resize", () => {
   document.body.classList.add("resize-animation-stopper");
   clearTimeout(resizeTimer);
@@ -16,12 +34,3 @@ window.addEventListener("resize", () => {
     document.body.classList.remove("resize-animation-stopper");
   }, 400);
 });
-// const equalWidth = function () {
-//   let heroBody = document.querySelector(".hero__gibberish");
-//   let titleInfo = document.querySelector(".hero__title").getBoundingClientRect();
-//   let bodyInfo = heroBody.getBoundingClientRect();
-//   if (parseInt(bodyInfo.width) === parseInt(titleInfo.width)) return;
-//   else heroBody.style.maxWidth = `${titleInfo.width}px`;
-// };
-
-// window.addEventListener("resize", equalWidth);
